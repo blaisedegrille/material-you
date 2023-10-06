d9374381ed2a0e64d6d64ca3029a604cf48a1525
blaisedegrille <47615137+blaisedegrille@users.noreply.github.com>
2023-10-06 18:04:56 +0200

    material-3

---
+++ src/assets/javascript/index.js
@@ -0,0 +1,3 @@
+import "../../../node_modules/elevated-button.js";
+import "../../../node_modules/@material/web/button/outlined-button.js";
+import "../../../node_modules/@material/web/checkbox/checkbox.js";
